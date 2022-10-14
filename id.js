f63d17c730affc5932625c4b25ef36176538260c
overwatch-league <115841588+overwatch-league@users.noreply.github.com>
2022-10-15 00:02:24 +0200

    Update id.js

--- id.js
+++ id.js
@@ -6,7 +6,7 @@ console.log(code);
 async function getAccessToken() {
 let headers = new Headers();
 await headers.append('Authorization', 'Basic ' + btoa('26a5a476b9064ba990689221ffc120ae' + ":" + 'ijdMG8TGWT4bi4kWWOTuAA9fqhgd8ucF'));
-await headers.append('redirect_uri', 'https://localhost');
+await headers.append('redirect_uri', 'https://overwatch-league.github.io/id.html');
 await headers.append('scope' , 'openid');
 await headers.append('grant_type', 'authorization_code');
 await headers.append('code', code);
