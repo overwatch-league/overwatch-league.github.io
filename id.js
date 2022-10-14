d8e906b27baa7a64e5584670591a3a29548de15e
overwatch-league <115841588+overwatch-league@users.noreply.github.com>
2022-10-15 00:41:01 +0200

    Update id.js

--- id.js
+++ id.js
@@ -5,13 +5,14 @@ console.log(code);
 
 async function getAccessToken() {
   
-const accessRequest = await fetch('https://oauth.battle.net/token', { 
-method: 'POST', headers: {
-'Authorization': 'Basic ' + btoa('26a5a476b9064ba990689221ffc120ae' + ":" + 'ijdMG8TGWT4bi4kWWOTuAA9fqhgd8ucF'),
-'redirect_uri': 'https://overwatch-league.github.io/id.html',
-'scope': 'openid',
-'grant_type': 'authorization_code',
-'code': code}});
+const accessRequest = fetch("https://oauth.battle.net/token", {
+  body: "redirect_uri=https://overwatch-league.github.io/id.html&scope=openid&grant_type=authorization_code&code=" + code,
+  headers: {
+    Authorization: "Basic MjZhNWE0NzZiOTA2NGJhOTkwNjg5MjIxZmZjMTIwYWU6aWpkTUc4VEdXVDRiaTRrV1dPVHVBQTlmcWhnZDh1Y0Y=",
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  method: "POST"
+})
 
 const response = await accessRequest.json();
 console.log(response);
