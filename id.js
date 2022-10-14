f9ba6ced3c8d37ab229b165950ec275aab7ef363
overwatch-league <115841588+overwatch-league@users.noreply.github.com>
2022-10-14 23:42:22 +0200

    Create id.js

---
+++ id.js
@@ -0,0 +1,21 @@
+const queryString = window.location.search;
+const args = new URLSearchParams(queryString);
+const code = args.get('code');
+
+async function getAccessCode() {
+let headers = new Headers();
+await headers.append('Authorization', 'Basic ' + btoa('26a5a476b9064ba990689221ffc120ae' + ":" + 'ijdMG8TGWT4bi4kWWOTuAA9fqhgd8ucF'));
+await headers.append('redirect_uri', 'https://localhost');
+await headers.append('scope' , 'openid');
+await headers.append('grant_type', 'authorization_code');
+await headers.append('code', code);
+  
+  const accessRequest = await fetch(url, {
+  method: 'POST', headers: headers });
+  
+  const response = accessRequest.json();
+  
+  console.log(response);
+  
+}
+ 
