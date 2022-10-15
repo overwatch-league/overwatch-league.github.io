dd87b26daaadc541c1d549b4964747325d512062
overwatch-league <115841588+overwatch-league@users.noreply.github.com>
2022-10-15 02:02:29 +0200

    Update id.js

--- id.js
+++ id.js
@@ -5,32 +5,41 @@ console.log(code);
 
 async function getAccessToken() {
 
-const params = await new URLSearchParams();
-params.append('redirect_uri', 'https://overwatch-league.github.io/id.html');
-params.append('scope', 'openid');
-params.append('grant_type', 'authorization_code');
-params.append('code', code);
+  const params = await new URLSearchParams();
+  params.append('redirect_uri', 'https://overwatch-league.github.io/id.html');
+  params.append('scope', 'openid');
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code);
 
-const request = await fetch("https://oauth.battle.net/token", {
-  body: params,
-  method: "POST",
-  headers: {
-    Authorization: "Basic MjZhNWE0NzZiOTA2NGJhOTkwNjg5MjIxZmZjMTIwYWU6aWpkTUc4VEdXVDRiaTRrV1dPVHVBQTlmcWhnZDh1Y0Y=",
-    "Content-Type": "application/x-www-form-urlencoded"
-  }});
+  const request = await fetch("https://oauth.battle.net/token", {
+    body: params,
+    method: "POST",
+    headers: {
+      Authorization: "Basic MjZhNWE0NzZiOTA2NGJhOTkwNjg5MjIxZmZjMTIwYWU6aWpkTUc4VEdXVDRiaTRrV1dPVHVBQTlmcWhnZDh1Y0Y=",
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
 
-const response = await request.json();
-console.log(response);
-console.log(request);
-  
-//GET ID
+  const response = await request.json();
+  console.log(response);
+  console.log(request);
 
-response = JSON.parse(response);
-const access_token = response.access_token;
+  document.getElementById('debug').innerHTML =
+    'DEBUG (IGNORE IF UNSURE): \n' + JSON.stringify(response);
 
-const request2 = await fetch('https://oauth.battle.net/userinfo', { headers: {'Authorization': 'Bearer ' + access_token}});
+  //GET ID
 
-const response2 = await request2.json();
-console.log(response2);
+  const access_token = response.access_token;
 
+  const requestId = await fetch('https://oauth.battle.net/userinfo', { headers: { 'Authorization': 'Bearer ' + access_token } });
+
+  const responseId = await requestId.json();
+  console.log(responseId);
+
+  document.getElementById('id_text').innerHTML = 'Your Account Id is:';
+  document.getElementById('account_id').innerHTML = responseId.id + (" ("+ responseId.battletag + ")");
+
+  //DEBUG
+  document.getElementById('debug').innerHTML +=
+   '\n' + JSON.stringify(responseId);
 }
